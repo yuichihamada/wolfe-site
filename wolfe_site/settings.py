179684767d056e849fa038b55e4cee014eea771b
yuichihamada <ilovemrchildrensazan@gmail.com>
2025-11-09 17:09:50 +0900

    初回コミット

--- wolfe_site/settings.py
+++ wolfe_site/settings.py
@@ -59,6 +59,7 @@ DATABASES = {
 }
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'content' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
