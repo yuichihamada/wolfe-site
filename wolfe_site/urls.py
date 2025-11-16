b1bb1655bf09215be660077de45845aad3cf44d4
yuichihamada <ilovemrchildrensazan@gmail.com>
2025-11-16 23:58:27 +0900

    ・一時的に “migration専用の管理コマンドビュー” を作成

--- wolfe_site/urls.py
+++ wolfe_site/urls.py
@@ -2,10 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.core.management import call_command
+
+def run_migrate(request):
+    call_command("migrate")
+    call_command("createsuperuser", 
+                 username="admin",
+                 email="admin@example.com",
+                 interactive=False)
+    return HttpResponse("Migration & superuser done.")
 
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('', include('content.urls')),
+  path("run-migrate/", run_migrate),
 ]
 
 if settings.DEBUG:
